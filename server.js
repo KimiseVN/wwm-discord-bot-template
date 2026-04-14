9aa6f966a30091cd76c3aabcca6f87310198064f
KimiseVN <198150635+KimiseVN@users.noreply.github.com>
2026-04-14 17:39:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+import express from "express";
+const app = express();
 
+// Simple keep-alive endpoint for Replit + UptimeRobot
+app.get("/", (req, res) => {
+  res.send("WWM Discord Bot is running!");
+});
+
+// Replit provides PORT automatically
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`[KEEP-ALIVE] Server running on port ${port}`);
+});
